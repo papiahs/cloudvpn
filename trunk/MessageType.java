a1feaf064748b008c3e92031374ab765f78f7cea
mr.alshalan@gmail.com <mr.alshalan@gmail.com@1734a6e8-bbe5-6009-56b8-12033e3016b0>
2011-05-04 01:48:00 +0000

    v0.1

---
+++ trunk/MessageType.java
@@ -0,0 +1,9 @@
+package edu.asu.eas.snac.cloudvpn.messages;
+
+public class MessageType {
+	public static final int FEEDBACK_MSG = 0;
+	public static final int LOGIN_MSG = 1;
+	public static final int REGISTER_MSG = 2;
+}
+
+
