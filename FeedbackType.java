f1b5b12ab2df148340f180a45081f8308018fcbc
mr.alshalan <mr.alshalan@gmail.com>
2011-05-04 01:46:49 +0000

    v0.1

---
+++ FeedbackType.java
@@ -0,0 +1,9 @@
+package edu.asu.eas.snac.cloudvpn.messages;
+
+public class FeedbackType {
+	public static final int SUCCESS = 0;
+	public static final int WRONG_USER_PASS = 1;
+	public static final int USER_EXIST = 2;
+	public static final int REGISTURE_FAILURE = 2;
+	
+}
